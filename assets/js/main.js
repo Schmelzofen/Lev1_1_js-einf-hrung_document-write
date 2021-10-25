c2fef5655457613bb3ab439457add7da56847068
Schmelzofen <sertac.oezdemir@gmail.com>
2021-10-25 20:27:06 +0200

    document write

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+document.write("<h1>Hello World!</h1> <p>Have a nice day!</p>");
+
+var myText = "Hello again <br><br>"
+
+document.write(myText)
+
+var Name = "Sertac"
+var nachname = "Özdemir"
+
+document.write("Name: " + Name + "<br>")
+document.write("Nachname: " + nachname)
